5d150436441e745dcb51bf2377918732e2d5f39d
lisa <121300266+jihyelisa@users.noreply.github.com>
2023-01-02 18:20:27 +0900

    Add files via upload

--- keras/keras01.py
+++ keras/keras01.py
@@ -14,7 +14,8 @@ from tensorflow.keras.layers import Dense   #구성 형태
 # tensorflow 안의 keras 안의 layers 안의 Dense를 가져옴
 
 model = Sequential()
-model.add(Dense(1, input_dim=1))   #dim: dimension
+model.add(Dense(1, input_dim=1))
+#dim: dimension > 하나의 차원, 하나의 덩어리가 들어간다는 의미
 #1 : y값 행렬데이터 덩어리, input_dim=1 : x값 행렬데이터 덩어리
 #"정제된" 데이터를 입력해야 함
 
@@ -26,6 +27,7 @@ model.compile(loss='mae', optimizer='adam')   #참고) mae: minimum average erro
 
 ##3-2. 훈련
 model.fit(x, y, epochs=2000)   #epochs: 훈련횟수
+#많이 훈련하면 최적의 w를 구할 수 있지만 지나치게 많이 하면 과적합될 수 있음
 
 
 ##4. 평가, 예측
---
+++ keras/keras04_deep.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+#1. 데이터
+x = np.array([1,2,3,4,5])
+y = np.array([1,2,3,5,4])
+
+#2. 모델구성
+model = Sequential()
+model.add(Dense(50, input_dim=1))
+model.add(Dense(40))
+model.add(Dense(30))
+model.add(Dense(20))
+model.add(Dense(10))
+model.add(Dense(5))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mae', optimizer='adam')
+model.fit(x, y, epochs=500)
+
+#4. 평가, 예측
+result = model.predict([6])
+print('6의 결과:', result)
+
+
