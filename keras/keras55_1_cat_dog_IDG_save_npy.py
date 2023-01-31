eb0514710a1646ee4986daa6593ff6388ff31594
jihyelisa <jihyelisak@gmail.com>
2023-01-31 20:53:21 +0900

    Day 021 | numpy.save(), num.load(), 이미지 데이터 증폭하기

--- keras/keras55_1_cat_dog_IDG_save_npy.py
+++ keras/keras55_1_cat_dog_IDG_save_npy.py
@@ -4,14 +4,17 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 train_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-# xy_train = train_datagen.flow_from_directory(
-#     'D:/_data/dogs-vs-cats/train/',  
-#     target_size=(100, 100),
-#     batch_size=26000,  # 너무 큰 수를 주면 memory overflow 발생
-#     class_mode='binary',
-#     color_mode='rgb',
-#     shuffle=True,
-# )  # Found 25000 images belonging to 2 classes.
+xy_train = train_datagen.flow_from_directory(
+    'D:/_data/dogs-vs-cats/train/',
+    classes=['cat','dog'],
+    # cat, dog directory 순서로 label 0,1을 설정
+    # 선생님은 생략하셨음
+    target_size=(100, 100),
+    batch_size=26000,  # 너무 큰 수를 주면 memory overflow 발생
+    class_mode='categorical',
+    color_mode='rgb',
+    shuffle=True,
+)  # Found 25000 images belonging to 2 classes.
 
 # xy_test = test_datagen.flow_from_directory(
 #     'D:/_data/dogs-vs-cats/test/',
@@ -25,14 +28,14 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 test_img = test_datagen.flow_from_directory(
     'D:/_data/dogs-vs-cats/test_img/',
     target_size=(100, 100),
-    batch_size=2,
-    class_mode='binary',
+    batch_size=6,
+    class_mode='categorical',
     color_mode='rgb',
     shuffle=True,
 )
 
-# np.save('./_data/dogs-vs-cats/dog_cat_x_train_100.npy', arr=xy_train[0][0])
-# np.save('./_data/dogs-vs-cats/dog_cat_y_train_100.npy', arr=xy_train[0][1])
+# np.save('./_data/dogs-vs-cats/dog_cat_x_train.npy', arr=xy_train[0][0])
+# np.save('./_data/dogs-vs-cats/dog_cat_y_train.npy', arr=xy_train[0][1])
 # np.save('./_data/dogs-vs-cats/dog_cat_x_test_100.npy', arr=xy_test[0][0])
 # np.save('./_data/dogs-vs-cats/dog_cat_y_test_100.npy', arr=xy_test[0][1])
 np.save('./_data/dogs-vs-cats/test_img.npy', arr=test_img[0][0])
--- keras/keras55_2_cat_dog_IDG_load_npy.py
+++ keras/keras55_2_cat_dog_IDG_load_npy.py
@@ -11,6 +11,13 @@ test_img = np.load('./_data/dogs-vs-cats/test_img.npy')
 # print(x_test.shape, y_test.shape)
 # (12500, 100, 100, 3) (12500,)
 
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test \
+    = train_test_split(x_train, y_train,
+                       test_size = 0.15, random_state = 13,
+                       stratify = y_train)
+
 
 
 ##2. 모델
@@ -18,48 +25,68 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten
 
 model = Sequential()
-model.add(Conv2D(30, 30, input_shape=(100, 100, 3)))
-model.add(Conv2D(80, 20, activation='relu'))
-model.add(Conv2D(40, 20, activation='relu'))
+model.add(Conv2D(30, 3, input_shape=(100, 100, 3)))
+model.add(Conv2D(30, 3, activation='relu'))
+model.add(Conv2D(20, 3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(20, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(2, activation='softmax'))
 
 
 
 ##3. 컴파일, 훈련
-model.compile(loss='binary_crossentropy',
+model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam', metrics=['acc'])
 
 hist = model.fit(x_train, y_train,
-                 batch_size=80, epochs=5,
-                 validation_split=0.2,
+                 batch_size=290, epochs=4,
+                 validation_data=[x_test, y_test],
                  verbose=3)
 
 
 
 ##4. 평가, 예측
-# loss = hist.history['loss']
-# val_loss = hist.history['val_loss']
+loss = hist.history['loss']
+val_loss = hist.history['val_loss']
 accuracy = hist.history['acc']
 val_acc = hist.history['val_acc']
 
-# print('loss:', loss[-1])
-# print('val_loss:', val_loss[-1])
+print('loss:', loss[-1])
+print('val_loss:', val_loss[-1])
 print('accuracy:', accuracy[-1])
 print('val_acc:', val_acc[-1])
 
 
 test = model.predict(test_img)
-if test == 0:
-    print('cat')
-else:
-    print('dog')
+test = np.argmax(test, axis=1)
+print(test)
+
+result = []
+for i in range(5):
+    if test[i] == 0:
+        result.append('cat')
+    else:
+        result.append('dog')
+print(f'We have {result}')
+print(f'\nAnd Ddiyong is a good {result[0]}.')
+
+
+
+
+
+import matplotlib.pyplot as plt
+import matplotlib
+
+plt.figure(figsize = (12,8))
+
+for each in ['loss', 'val_loss', 'acc', 'val_acc']:
+    plt.plot(hist.history[each], label = each)
+    
+plt.legend()
+plt.grid()
+plt.show()
 
 
 '''
-loss: 3.6270205328037264e-06
-val_loss: 0.023630639538168907
-accuracy: 1.0
-val_acc: 0.9916666746139526
+
 '''
